ec863bc8121de6eb5d220c0c43e16556ab4c6428
Herman <hermansyah_@live.com>
2026-03-11 22:00:15 +0700

    Enhance tenant profile management and password reset features
    - Improved form validation and loading states for the password reset functionality in the AdminTenantDetail view.
    - Added a new route and view for admin profile management, enabling updates to admin profile information.
    - Updated the admin API to facilitate profile retrieval and updates, including password changes.
    - Enhanced the admin layout to incorporate a link to the new profile management section.
    - Strengthened error handling and user feedback for both password reset and profile update operations.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b400efb0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.hk7a56g22ao"
+    "revision": "0.qkg30r4ihvg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
