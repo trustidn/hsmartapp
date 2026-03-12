3956152ed420552acc3cc846649d6b4a09235942
Herman <hermansyah_@live.com>
2026-03-12 16:41:57 +0700

    Refactor sales item handling and improve custom item input experience
    - Updated CreateSaleItemInput to enhance support for custom items, including optional ProductID and added ProductName.
    - Enhanced sales repository methods to accommodate custom items and improved service layer for product ID parsing.
    - Updated POS store interface to streamline the addition of custom items with a dedicated input modal.
    - Improved product ranking queries to include custom items in sales reports, enhancing data accuracy.
    - Refined frontend layout for better user experience when inputting custom items.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b400efb0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.rfimp42n74o"
+    "revision": "0.ols1304k3pg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
